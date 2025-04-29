06b2f8b055e2cd568b33560f2dc257e7fe0acc9c
Abdul Mannan <mannan.jnv123@gmail.com>
2025-04-29 19:30:18 +0530

    added basic login

--- app/api/scrape/route.js
+++ app/api/scrape/route.js
@@ -114,7 +114,7 @@ async function launchBrowser() {
   }
 
   return await puppeteer.launch({
-    headless: true,
+    headless: false,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
